e8102979e2d193477058f195e2c8e00bbffee96b
ramdhadage <ram.dhadage123@gmail.com>
2023-11-25 22:11:59 +0530

    Added shiny namepace in app.R

--- app.R
+++ app.R
@@ -2,16 +2,16 @@
 #'
 #' @import shiny
 # Suppressing unused warnings
-options(warn=-1)
+options(warn = -1)
 
-options(shiny.autoload.r=FALSE)
+options(shiny.autoload.r = FALSE)
 
 # Load all files and folders from Biodiversity Dashboard to run the app
 
 pkgload::load_all()
 
 app_ui <- function(request) {
-  tagList(
+  shiny::tagList(
     # build the ui
     createPage(
       title = "Biodiversity Dashboard",
@@ -26,26 +26,29 @@ app_ui <- function(request) {
 #'     DO NOT REAMOVE.
 #' @import shiny
 app_server <- function(input, output, session) {
-
   # initiates All inputs values to NULL
-  inputList <- shiny::reactiveValues(
-    radioBtn_searchByName = NULL,
-    searchByVerOrSciName = NULL, # this will returns the searched input values as Species Name by Vernacular or Scientific based on  seach by species radio button choices
-    selectedBySciOrVerName =  NULL # this will returns the  selectedinput values as Species Name by Vernacular or Scientific based on  seach by species radio button choices
+  input_list <- shiny::reactiveValues(
+    radioBtn_searchByName = NULL, searchByVerOrSciName = NULL,
+    # this will returns the searched input values as Species Name by
+    # Vernacular or Scientific based on  seach by species radio button choices
+
+    selectedBySciOrVerName = NULL
+    # this will returns the  selectedinput values as Species Name by
+    # Vernacular or Scientific based on  seach by species radio button choices
   )
 
   # create the inputs and store its inputs id for next modules
-  mod01_loadInputs_server("inputs",inputList)
+  mod01_loadInputs_server("inputs", input_list)
   shiny::observe({
     # added module to map the occurences using leaflet library
-    mod02_viewMap_server("map", inputList)
+    mod02_viewMap_server("map", input_list)
     # added third module to plot occurrences per month
-    mod03_timelineVisualization_server("timeline_plot", inputList,
-                                       flg_darkMode = reactive(input$dark_mode))
+    mod03_timelineVisualization_server("timeline_plot", input_list,
+      flg_darkMode = shiny::reactive(input$dark_mode)
+    )
   })
 }
 shiny::shinyApp(
   ui = app_ui,
   server = app_server,
 )
-
