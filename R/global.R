67801d83215e49c5f3330cd3625bf0e457fc4386
ramdhadage <ram.dhadage123@gmail.com>
2023-11-25 22:11:35 +0530

    added options(shiny.autoload.r=FALSE) to resolved Warning in loadSupport(appDir, renv = sharedEnv, globalrenv = NULL) : Loading R/ subdirectory for Shiny.

--- R/global.R
+++ R/global.R
@@ -1,3 +1,5 @@
 # adding disk cache location
-shinyOptions(cache = cachem::cache_disk("./app_cache/cache/"))
+shiny::shinyOptions(cache = cachem::cache_disk("./app_cache/cache/"))
 # shiny suppress unwanted warning
+
+options(shiny.autoload.r=FALSE)
