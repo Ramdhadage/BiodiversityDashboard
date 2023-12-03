a809353032a79c886b0852fca4883877235e76af
ramdhadage <ram.dhadage123@gmail.com>
2023-12-03 21:37:28 +0530

    Use bslib labrary instead of bs4Dash for dashboard building.

--- app.R
+++ app.R
@@ -1,54 +1,22 @@
-#' The application User-Interface
-#'
-#' @import shiny
-# Suppressing unused warnings
-options(warn = -1)
-
-options(shiny.autoload.r = FALSE)
-
-# Load all files and folders from Biodiversity Dashboard to run the app
-
-pkgload::load_all()
-
-app_ui <- function(request) {
-  shiny::tagList(
-    # build the ui
-    createPage(
-      title = "Biodiversity Dashboard",
-      subtitle = "",
-      context = ""
-    )
-  )
-}
-#' The application server-side
-#'
-#' @param input,output,session Internal parameters for {shiny}.
-#'     DO NOT REAMOVE.
-#' @import shiny
-app_server <- function(input, output, session) {
-  # initiates All inputs values to NULL
-  input_list <- shiny::reactiveValues(
-    radioBtn_searchByName = NULL, searchByVerOrSciName = NULL,
-    # this will returns the searched input values as Species Name by
-    # Vernacular or Scientific based on  seach by species radio button choices
-
-    selectedBySciOrVerName = NULL
-    # this will returns the  selectedinput values as Species Name by
-    # Vernacular or Scientific based on  seach by species radio button choices
+library(shiny)
+devtools::load_all()
+ui <- bslib::page_sidebar(
+  title = "Biodiversity Dashboard",
+  sidebar = bslib::sidebar(
+    mod01_search_species_by_ui("input")
+  ),
+  bslib::card(
+    full_screen = TRUE,
+    mod02_viewMap_ui("map")
   )
 
-  # create the inputs and store its inputs id for next modules
-  mod01_loadInputs_server("inputs", input_list)
-  shiny::observe({
-    # added module to map the occurences using leaflet library
-    mod02_viewMap_server("map", input_list)
-    # added third module to plot occurrences per month
-    mod03_timelineVisualization_server("timeline_plot", input_list,
-      flg_darkMode = shiny::reactive(input$dark_mode)
-    )
-  })
-}
-shiny::shinyApp(
-  ui = app_ui,
-  server = app_server,
 )
+
+server <- function(input, output, session) {
+
+  input_list <- mod01_search_species_by_Server("input")
+  mod02_viewMap_server("map", input_list)
+
+}
+
+shinyApp(ui, server)
