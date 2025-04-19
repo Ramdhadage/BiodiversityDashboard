580ed34a625bf4089090818dd942dff201b32f14
ramdhadage <ram.dhadage123@gmail.com>
2025-04-19 23:28:07 +0530

    fixed the issue that app is not creating map and timeline plot at begining.

--- R/mod01_loadInputs.R
+++ R/mod01_loadInputs.R
@@ -22,9 +22,7 @@ mod01_loadInputs_ui <- function(id) {
           "Scientific Name"
         ),
         checkIcon = list(
-          yes = icon("ok",
-            lib = "glyphicon"
-          )
+          yes = icon("ok",lib = "glyphicon")
         ),
         justified = TRUE
       ),
@@ -41,7 +39,7 @@ mod01_loadInputs_server <- function(id, inputList) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    # Create a HTML OUtput Tags for searchable drop down for search species by Scientific or Vernacular name based on Search by species radio button.
+    # Create a HTML Output Tags for searchable drop down for search species by Scientific or Vernacular name based on Search by species radio button.
     output$mini_ui1 <- shiny::renderUI({
       # Create a searchable drop down for search species by Scientific or Vernacular name based on Search by species radio button.
       dropdownBasedOnRadioBtn(
@@ -51,9 +49,9 @@ mod01_loadInputs_server <- function(id, inputList) {
       )
     })
 
-    # Creates HTML Output tags for searchable drop down for selected species by Scientific or Vernacular name based  on Search species by Scientific or Vernacular Name.
+    # Creates HTML Output tags for searchable drop down for selected species by Scientific or Vernacular name based on Search species by Scientific or Vernacular Name.
     output$mini_ui2 <- shiny::renderUI({
-      # Create searchable drop down for selected species by Scientific or Vernacular name based  on Search species by Scientific or Vernacular Name.
+      # Create searchable drop down for selected species by Scientific or Vernacular name based on Search species by Scientific or Vernacular Name.
       dropdownBasedOnRadioBtn(
         radioBtn_search_byName = input$search_byName, radioBtn_search_byNameID = "Vernacular Name",
         id1 = ns("selected_sci_name"), label1 = "Matching Scientific Name of Searched Species: ",
@@ -61,9 +59,29 @@ mod01_loadInputs_server <- function(id, inputList) {
         choices2 = NULL
       )
     })
+
+    observe({
+      # Always update the radio button value, even if NULL
+      inputList$radioBtn_searchByName <- input$search_byName
+
+      # Update searchByVerOrSciName based on radio button selection
+      if (identical(input$search_byName, "Vernacular Name")) {
+        inputList$searchByVerOrSciName <- input$ver_name
+      } else if (identical(input$search_byName, "Scientific Name")) {
+        inputList$searchByVerOrSciName <- input$sci_name
+      }
+
+      # Update selectedBySciOrVerName based on radio button selection
+      if (identical(input$search_byName, "Vernacular Name") && !is.null(input$selected_sci_name)) {
+        inputList$selectedBySciOrVerName <- input$selected_sci_name
+      } else if (identical(input$search_byName, "Scientific Name") && !is.null(input$selected_ver_name)) {
+        inputList$selectedBySciOrVerName <- input$selected_ver_name
+      }
+    })
     # Added combined reactive values for adding in binding event
-    bindEventExpression <- shiny::reactive(list(input$ver_name, input$search_byName, input$sci_name))
-    shiny::observe({
+    bindEventExpression <- reactive(list(input$ver_name, input$search_byName, input$sci_name))
+
+    observeEvent(bindEventExpression(), {
       updateDropdownBasedOnRadioBtnValues(
         radioBtn_search_byName = input$search_byName,
         radioBtn_search_byNameID_ver = "Vernacular Name", id_ver_name = input$ver_name,
@@ -71,28 +89,15 @@ mod01_loadInputs_server <- function(id, inputList) {
         radioBtn_search_byNameID_sci = "Scientific Name", id_sci_name = input$sci_name,
         id_selected_ver_name = input$selected_ver_name, session = session
       )
+    })
 
-      # Updating InptList with updated inputvalues as species name and Scientific and Vernacular Name
-      inputList$radioBtn_searchByName <- input$search_byName
-      req(input$ver_name, input$sci_name) # added req to resolve the issue if_else: `true` must be a vector, not `NULL`
-      inputList$searchByVerOrSciName <- if_else(input$search_byName == "Vernacular Name",
-        input$ver_name, input$sci_name
-      )
-      inputList$selectedBySciOrVerName <- if_else(input$search_byName == "Vernacular Name" & !is.null(input$selected_sci_name),
-        input$selected_sci_name, input$selected_ver_name
-      )
-    }) %>% shiny::bindEvent(bindEventExpression())
-    # update the input values of search species by Scientific or Vernacular Name into inputList.
-    bindEventExpression_selectedSpecies <- reactive(list(input$selected_sci_name, input$selected_ver_name))
-    shiny::observeEvent(bindEventExpression_selectedSpecies(), {
-      req(input$ver_name, input$sci_name) # added req to resolve the issue if_else: `true` must be a vector, not `NULL`
-      inputList$searchByVerOrSciName <- if_else(input$search_byName == "Vernacular Name",
-        input$ver_name, input$sci_name
-      )
-      inputList$selectedBySciOrVerName <- if_else(input$search_byName == "Vernacular Name" & !is.null(input$selected_sci_name),
-        input$selected_sci_name, input$selected_ver_name
-      )
-      inputList$radioBtn_searchByName <- input$search_byName
+    # Also observe changes to the selected names and update inputList
+    observeEvent(list(input$selected_sci_name, input$selected_ver_name), {
+      if (identical(input$search_byName, "Vernacular Name") && !is.null(input$selected_sci_name)) {
+        inputList$selectedBySciOrVerName <- input$selected_sci_name
+      } else if (identical(input$search_byName, "Scientific Name") && !is.null(input$selected_ver_name)) {
+        inputList$selectedBySciOrVerName <- input$selected_ver_name
+      }
     })
   })
 }
